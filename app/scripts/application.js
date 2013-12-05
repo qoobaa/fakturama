645213a98bfc5dfe80a82e1c457cc86c53927ee2
Kuba Kuźma <kuba@jah.pl>
2013-12-05 23:14:25 +0100

    added integerize

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,1 +1,21 @@
 var Faktura = Ember.Application.create();
+
+Ember.TextField.reopen({ attributeBindings: ["min", "max", "step"] });
+
+String.prototype.integerize = function (decimalPlaces) {
+    var integerPart, fractionalPart,
+        parts = this.replace(",", ".").split(".");
+
+    if (arguments.length == 0) {
+        decimalPlaces = 2;
+    }
+
+    integerPart = (parts[0] || "0");
+    fractionalPart = (parts[1] || "").substr(0, decimalPlaces);
+
+    while (fractionalPart.length < decimalPlaces) {
+        fractionalPart += "0";
+    }
+
+    return parseInt(integerPart + fractionalPart, 10);
+};
--- app/scripts/models/item.js
+++ app/scripts/models/item.js
@@ -1,10 +1,46 @@
 Faktura.Item = Ember.Object.extend({
     description: "Programowanie w języku Ruby / JavaScript",
-    quantity: 10,
-    unit: "hours",
-    netPrice: 110,
-    netAmount: 1100,
-    taxRate: 23,
-    taxAmount: 0,
+
+    formattedQuantity: "",
+
+    quantity: function () {
+        return this.get("formattedQuantity").integerize(3);
+    }.property("formattedQuantity"),
+
+    unit: undefined,
+
+    formattedNetPrice: "",
+
+    netPrice: function () {
+        return this.get("formattedNetPrice").integerize(2);
+    }.property("formattedNetPrice"),
+
+    formattedGrossPrice: "",
+
+    grossPrice: function () {
+        return this.get("formattedGrossPrice").integerize(2);
+    }.property("formattedGrossPrice"),
+
+    formattedNetAmount: function () {
+        return this.get("netAmount");
+    }.property("netAmount"),
+
+    netAmount: function (key, value) {
+        return this.get("netPrice") * this.get("quantity") / 1000;
+    }.property("netPrice", "quantity"),
+
+    formattedTaxRate: "",
+
+    taxRate: function () {
+        var value = parseInt(this.get("taxRate"), 10);
+        return isNaN(value) ? 0 : value;
+    }.property("formattedTaxRate"),
+
+    formattedTaxAmount: "0",
+
+    taxAmount: function () {
+        return Math.round(this.get("netAmount") * this.get("taxRateValue") / 100);
+    }.property("netAmount", "taxRateValue"),
+
     grossAmount: 1100
 });
