e4859f7697ecb7a7e5a46a0aa8ad6a8b1fa8be1f
Kuba Kuźma <kuba@jah.pl>
2014-03-12 18:57:06 +0100

    added nameEN to units

--- app/models/unit.js
+++ app/models/unit.js
@@ -1,25 +1,26 @@
 var Unit = Ember.Model.extend({
     code: Ember.attr(),
-    name: Ember.attr()
+    name: Ember.attr(),
+    nameEN: Ember.attr()
 });
 
 Unit.reopenClass({
     adapter: Ember.FixtureAdapter.create(),
     primaryKey: "code",
     FIXTURES: [
-        {code: "hour", name: "godz."},
-        {code: "service", name: "usł."},
-        {code: "piece", name: "szt."},
-        {code: "day", name: "dni"},
-        {code: "discount", name: "rabat"},
-        {code: "kilogram", name: "kg"},
-        {code: "ton", name: "ton"},
-        {code: "metre", name: "m"},
-        {code: "kilometre", name: "km"},
-        {code: "advance", name: "zaliczka"},
-        {code: "set", name: "komplet"},
-        {code: "squaremetre", name: "m²"},
-        {code: "cubicmetre", name: "m³"}
+        {code: "hour", name: "godz.", nameEN: "hrs"},
+        {code: "service", name: "usł.", nameEN: "service"},
+        {code: "piece", name: "szt.", nameEN: "pcs"},
+        {code: "day", name: "dni", nameEN: "days"},
+        {code: "discount", name: "rabat", nameEN: "discount"},
+        {code: "kilogram", name: "kg", nameEN: "kg"},
+        {code: "ton", name: "ton", nameEN: "tons"},
+        {code: "metre", name: "m", nameEN: "m"},
+        {code: "kilometre", name: "km", nameEN: "km"},
+        {code: "advance", name: "zaliczka", nameEN: "advance"},
+        {code: "set", name: "komplet", nameEN: "set"},
+        {code: "squaremetre", name: "m²", nameEN: "m²"},
+        {code: "cubicmetre", name: "m³", nameEN: "m³"}
     ]
 });
 
