53c92fadcf6b9a56bd1cda74e3cc496ba3e4a030
Kuba Kuźma <kuba@jah.pl>
2014-03-12 19:13:52 +0100

    added styles

--- app/models/tax_rate.js
+++ app/models/tax_rate.js
@@ -1,19 +1,20 @@
 var TaxRate = Ember.Model.extend({
     code: Ember.attr(),
     value: Ember.attr(),
-    name: Ember.attr()
+    name: Ember.attr(),
+    nameEN: Ember.attr()
 });
 
 TaxRate.reopenClass({
     adapter: Ember.FixtureAdapter.create(),
     primaryKey: "code",
     FIXTURES: [
-        { code: "23",     value: 23, name: "23%"  },
-        { code: "8",      value:  8, name: "8%"   },
-        { code: "5",      value:  5, name: "5%"   },
-        { code: "0",      value:  0, name: "0%"   },
-        { code: "na",     value:  0, name: "n.p." },
-        { code: "exempt", value:  0, name: "zw."  }
+        { code: "23",     value: 23, name: "23%",  nameEN: null },
+        { code: "8",      value:  8, name: "8%",   nameEN: null },
+        { code: "5",      value:  5, name: "5%",   nameEN: null },
+        { code: "0",      value:  0, name: "0%",   nameEN: null },
+        { code: "na",     value:  0, name: "n.p.", nameEN: "n/a" },
+        { code: "exempt", value:  0, name: "zw.",  nameEN: "exempt" }
     ]
 });
 
