2c9bd6490edd706da27daa5931f4bb98a8be10b0
Kuba Kuźma <kuba@jah.pl>
2014-04-06 23:01:35 +0200

    fixed tax calculation

--- app/lib/invoice_properties_mixin.js
+++ app/lib/invoice_properties_mixin.js
@@ -47,20 +47,46 @@ var InvoicePropertiesMixin = Ember.Mixin.create({
         return code && Language.find(code);
     }.property("languageCode"),
 
-    // totalNetAmount: function () {
-    //     return this.get("items").reduce(function (previousValue, item) {
-    //         return previousValue + item.get("netAmount");
-    //     }, 0);
-    // }.property("items", "items.@each.netAmount"),
+    subTotals: function () {
+        return this.get("items").mapBy("taxRate").uniq().map(function (taxRate) {
+            var items,
+                result = Ember.Object.create({ taxRate: taxRate });
 
-    netAmounts: Ember.computed.mapBy("subTotals", "netAmount"),
-    totalNetAmount: Ember.computed.sum("netAmounts"),
+            items = this.get("items").filterBy("taxRate", taxRate);
 
-    taxAmounts: Ember.computed.mapBy("subTotals", "taxAmount"),
-    totalTaxAmount: Ember.computed.sum("taxAmounts"),
+            result.set("netAmount", items.reduce(function (previousValue, item) {
+                return previousValue + item.get("netAmount");
+            }, 0));
 
-    grossAmounts: Ember.computed.mapBy("subTotals", "grossAmount"),
-    totalGrossAmount: Ember.computed.sum("grossAmounts"),
+            result.set("taxAmount", Math.round(result.get("netAmount") * result.get("taxRate.value") / 100));
+
+            result.set("grossAmount", result.get("netAmount") + result.get("taxAmount"));
+
+            if (this.get("exchangeRate")) {
+                result.set("taxAmountPLN", Math.round(result.get("taxAmount") * this.get("exchangeRate") / (this.get("exchangeDivisor") * 10000)));
+            }
+
+            return result;
+        }.bind(this));
+    }.property("items", "items.@each.netAmount", "items.@each.taxAmount", "items.@each.grossAmount", "items.@each.taxRate", "exchangeRate", "exchangeDivisor"),
+
+    totalNetAmount: function () {
+        return this.get("subTotals").reduce(function (previousValue, item) {
+            return previousValue + item.get("netAmount");
+        }, 0);
+    }.property("subTotals", "subTotals.@each.netAmount"),
+
+    totalTaxAmount: function () {
+        return this.get("subTotals").reduce(function (previousValue, item) {
+            return previousValue + item.get("taxAmount");
+        }, 0);
+    }.property("subTotals", "subTotals.@each.taxAmount"),
+
+    totalGrossAmount: function () {
+        return this.get("subTotals").reduce(function (previousValue, item) {
+            return previousValue + item.get("grossAmount");
+        }, 0);
+    }.property("subTotals", "subTotals.@each.grossAmount"),
 
     totalTaxAmountPLN: function () {
         if (this.get("isExchanging")) {
@@ -75,29 +101,6 @@ var InvoicePropertiesMixin = Ember.Mixin.create({
             return this.get("totalGrossAmount");
         }
     }.property("totalGrossAmount", "exchangeRate", "exchangeDivisor", "isForeignCurrency"),
-
-    subTotals: function () {
-        return this.get("items").mapBy("taxRate").uniq().map(function (taxRate) {
-            var items,
-                result = Ember.Object.create({ taxRate: taxRate });
-
-            items = this.get("items").filterBy("taxRate", taxRate);
-
-            result.netAmount = items.reduce(function (previousValue, item) {
-                return previousValue + item.get("netAmount");
-            }, 0);
-
-            result.taxAmount = Math.round(result.netAmount * result.taxRate.get("value") / 100);
-
-            result.grossAmount = result.netAmount + result.taxAmount;
-
-            if (this.get("isExchanging")) {
-                result.taxAmountPLN = Math.round(result.taxAmount * this.get("exchangeRate") / (this.get("exchangeDivisor") * 10000));
-            }
-
-            return result;
-        }.bind(this));
-    }.property("items", "items.@each.netAmount", "items.@each.taxAmount", "items.@each.grossAmount", "items.@each.taxRate", "isExchanging", "exchangeRate", "exchangeDivisor"),
 
     totalGrossAmountInWords: function () {
         var dollars, cents,
