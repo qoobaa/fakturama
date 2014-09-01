a69cb4615666d0f001dc0a936b8b22558ac99e5a
Kuba Kuźma <kuba@jah.pl>
2014-09-01 19:37:18 +0200

    updated bower stuff, added invoice title in preview

--- app/routes/invoice/show.js
+++ app/routes/invoice/show.js
@@ -1,6 +1,15 @@
 var InvoiceShowRoute = Ember.Route.extend({
     model: function (params) {
         return this.modelFor("invoice");
+    },
+    activate: function () {
+        var invoice = this.modelFor("invoice");
+
+        document.title = [invoice.get("number"), invoice.get("buyerFirstLine"), "- Fakturama"].compact().join(" ");
+    },
+    deactivate: function () {
+        document.title = "Fakturama";
+        console.log("deactivate", arguments);
     }
 });
 
