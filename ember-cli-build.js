88fb7d97cf4f6c1201d719d348a10f08e12e8a3b
Lucas Mendelowski <mendelowski@gmail.com>
2017-10-05 19:27:10 +0200

    Fix attributes and Bootstrap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,7 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    lessOptions: {
+      paths: [
+        'node_modules'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
